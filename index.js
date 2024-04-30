f692d8d9548cfab30c6c77af5301b4317e3017dc
saidscv23 <said.carrillo438@ist17dejulio.edu.ec>
2024-04-30 11:04:35 -0500

    Cambio de texto

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 
+//Uso Protocolo HTTP
 var http=require('http');
-// A traves del protocolo creamos un servidor y lo almacenamos en server
+// Creacion de un servidor usando HTTP
 var server=http.createServer();
 //metodo que perimite configurar
 function mensaje(petision,resp){
 //1. el estado del servidor a traves de un texto
 resp.writeHead(200,{'content-type':'text/plain'});
 //mensaje mostrar
-resp.write("BIENVENIDOS AMIGOS UGUUUUU");
+resp.write("BIENVENIDOS AMIGOS UwUUUUU");
 //Finaliza la respuesta
 resp.end();
-
 }
-
+//cuando tenga un requerimiento por parte de un cliente envie a presentar la funcion respuesta
 server.on('request',mensaje);
 server.listen(3000,function(){
-
 console.log('la consola esta corriendo');
-
-
 })
